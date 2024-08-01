92cd3d06b2f23db4dbf914fcb2cb78bbbe11947e
Frank <frankmendezresources@gmail.com>
2024-08-01 21:51:10 +0800

    fix: test

--- src/routes/router.test.tsx
+++ src/routes/router.test.tsx
@@ -3,10 +3,21 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 describe('Router', () => {
+    const queryClient = new QueryClient()
+
+    const routerComponent = () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
+        )
+    }
+
     it('renders the home page correctly', () => {
-        render(<RouterProvider router={router} />)
-        expect(screen.getByText('Home')).toBeInTheDocument()
+        routerComponent()
+        expect(screen.getByText('Trending')).toBeInTheDocument()
     })
 })
